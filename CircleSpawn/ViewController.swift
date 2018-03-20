74aea2a9c8b29ccb77c6263780c9d37309c3996d
Michał Czerniakowski <mpczerniakowski@gmail.com>
2018-03-20 17:33:36 +0100

    Remove storyboards

--- CircleSpawn/ViewController.swift
+++ CircleSpawn/ViewController.swift
@@ -1,25 +1,14 @@
-//
-//  ViewController.swift
-//  CircleSpawn
-//
-//  Created by Michał Czerniakowski on 20.03.2018.
-//  Copyright © 2018 Michał Czerniakowski. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    let view = View()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        
 
+    }
 
 }
-
