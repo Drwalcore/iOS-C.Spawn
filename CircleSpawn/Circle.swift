92f4a84c12a02847b02ff919f1c7cd604df1ce29
Michał Czerniakowski <mpczerniakowski@gmail.com>
2018-03-25 11:58:45 +0200

    Add animation, remove magic number.

--- CircleSpawn/Circle.swift
+++ CircleSpawn/Circle.swift
@@ -2,19 +2,29 @@ import UIKit
 
 class Circle: UIView {
 
-    let size: CGFloat = 100
+    private let kSize: CGFloat = 100
+    private let kInitialAlpha: CGFloat = 0
 
     init() {
-        let customFrame = CGRect(origin: .zero, size: CGSize(width: size, height: size))
+        let customFrame = CGRect(origin: .zero, size: CGSize(width: kSize, height: kSize))
         super.init(frame: customFrame)
         setupView()
+        addInitialAnimation()
     }
 
     private func setupView() {
         backgroundColor = UIColor.randomBrightColor()
-        layer.cornerRadius = size/2
-        alpha = 1
+        layer.cornerRadius = kSize/2
+        alpha = kInitialAlpha
+    }
 
+    private func addInitialAnimation() {
+        transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        let creationAnimator = UIViewPropertyAnimator(duration: 0.5, curve: .easeOut, animations: { [weak self] in
+            self?.alpha = 0.95
+            self?.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        })
+        creationAnimator.startAnimation()
     }
 
     required init?(coder aDecoder: NSCoder) {
