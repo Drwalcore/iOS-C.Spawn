6885a7830680260f454767ddb0c2df5c3720538a
Michał Czerniakowski <mpczerniakowski@gmail.com>
2018-03-25 15:18:01 +0200

    Fix circles removal for triple tap.

--- CircleSpawn/GameViewController.swift
+++ CircleSpawn/GameViewController.swift
@@ -9,7 +9,6 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         view = mainView
         setupSpawnGestureRecognizer()
-        setupTerminationGestureRecognizer()
         
         super.viewDidLoad()
     }
@@ -21,34 +20,24 @@ class GameViewController: UIViewController {
         mainView.addGestureRecognizer(doubleTapGestureRecognizer)
     }
 
-    private func setupTerminationGestureRecognizer() {
-        let tripleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tripleTapped(_:)))
-        tripleTapGestureRecognizer.numberOfTapsRequired = kNumbersOfTapsRequiredForTermination
-        mainView.circle.addGestureRecognizer(tripleTapGestureRecognizer)
-    }
-
     // MARK:- Gesture Recognizers Selectors
 
     @objc
     func doubleTapped(_ doubleTap: UITapGestureRecognizer) {
         let circle: Circle = Circle()
+        let tripleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tripleTapped(_:)))
+        tripleTapGestureRecognizer.numberOfTapsRequired = kNumbersOfTapsRequiredForTermination
+        tripleTapGestureRecognizer.delegate = self
+        circle.addGestureRecognizer(tripleTapGestureRecognizer)
         circle.center = doubleTap.location(in: mainView)
         mainView.addSubview(circle)
     }
 
     @objc func tripleTapped(_ tripleTap: UITapGestureRecognizer) {
         if let circleView = tripleTap.view {
-            let terminationAnimator = UIViewPropertyAnimator(duration: 0.5, curve: .easeIn, animations: {
-                circleView.alpha = 0
-                circleView.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
-            })
-            terminationAnimator.addCompletion { position in
-                if position == .end {
-                    circleView.removeFromSuperview()
-                }
-            }
-            terminationAnimator.startAnimation()
+            circleView.removeFromSuperview()
         }
+        return
     }
 
 }
